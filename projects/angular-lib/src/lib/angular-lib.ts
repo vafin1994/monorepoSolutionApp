461e5f1c8ea366f6f310472ef0918c6cdc9cc697
Oleksandr Vafin <vafinalex@gmail.com>
2026-05-20 19:14:27 +0200

    feat: fix the library build

--- projects/angular-lib/src/lib/angular-lib.ts
+++ projects/angular-lib/src/lib/angular-lib.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MyLibCore, MyLibCoreOptions } from '@monorepo/core';
 
 @Component({
   selector: 'lib-angular-lib',
   imports: [],
-  template: `
-    <p>
-      angular-lib works!
-    </p>
-  `,
+  template: `<p>{{ greeting }}</p>`,
   styles: ``,
 })
-export class AngularLib {
+export class AngularLib implements OnInit {
+  @Input() name = 'World';
+  @Input() options: MyLibCoreOptions = {};
 
+  greeting = '';
+
+  ngOnInit(): void {
+    const core = new MyLibCore(this.options);
+    this.greeting = core.greet(this.name);
+  }
 }
